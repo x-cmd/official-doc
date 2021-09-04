30e61fceacd033a6c8403ac82b96c1ee2aa6c0d3
Zhengqbbb <1074059947@qq.com>
2021-09-05 06:43:33 +0800

    fix(postcss): fix build css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./index.html', './docs/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: [
+    // './index.html',
+    // './docs/**/*.{vue,js,ts,jsx,tsx}'
+  ],
    darkMode: false, // or 'media' or 'class'
    theme: {
      extend: {},
