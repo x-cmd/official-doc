c4ba24b7cccc41c4e238bc16a184a0d4626d35b7
Zhengqbbb <1074059947@qq.com>
2021-09-05 06:26:38 +0800

    fix(postcss): add post css devDependencies

--- docs/.vuepress/configs/bunder/index.ts
+++ docs/.vuepress/configs/bunder/index.ts
@@ -1,11 +1,11 @@
 import type { AppOptions, WebpackChainConfig, WebpackConfiguration } from 'vuepress'
 const isProd = process.env.NODE_ENV === 'production'
 
-// export const bundler: AppOptions["bundler"] = // specify bundler via environment variable
-// process.env.DOCS_BUNDLER ??
-// // use vite in dev, use webpack in prod
-// (isProd ? '@vuepress/webpack' : '@vuepress/vite');
-export const bundler: AppOptions["bundler"] = '@vuepress/vite'
+export const bundler: AppOptions["bundler"] = // specify bundler via environment variable
+process.env.DOCS_BUNDLER ??
+// use vite in dev, use webpack in prod
+(isProd ? '@vuepress/webpack' : '@vuepress/vite');
+// export const bundler: AppOptions["bundler"] = '@vuepress/vite'
 
 export const bundlerConfig: AppOptions["bundlerConfig"] = {
   viteOptions: {
@@ -17,5 +17,13 @@ export const bundlerConfig: AppOptions["bundlerConfig"] = {
         ]
       }
     },
-  }
+  },
+  postcss: {
+    postcssOptions: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
 }
