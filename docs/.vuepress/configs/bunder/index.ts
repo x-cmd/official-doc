de416f943e308b4c72d2bb33a3f2fec3828dbfb1
Zhengqbbb <1074059947@qq.com>
2021-09-05 05:52:05 +0800

    chore(build): change build mode

--- docs/.vuepress/configs/bunder/index.ts
+++ docs/.vuepress/configs/bunder/index.ts
@@ -1,10 +1,11 @@
-import type { AppOptions, WebpackChainConfig } from 'vuepress'
+import type { AppOptions, WebpackChainConfig, WebpackConfiguration } from 'vuepress'
 const isProd = process.env.NODE_ENV === 'production'
 
-export const bundler: AppOptions["bundler"] = // specify bundler via environment variable
-process.env.DOCS_BUNDLER ??
-// use vite in dev, use webpack in prod
-(isProd ? '@vuepress/webpack' : '@vuepress/vite');
+// export const bundler: AppOptions["bundler"] = // specify bundler via environment variable
+// process.env.DOCS_BUNDLER ??
+// // use vite in dev, use webpack in prod
+// (isProd ? '@vuepress/webpack' : '@vuepress/vite');
+export const bundler: AppOptions["bundler"] = '@vuepress/vite'
 
 export const bundlerConfig: AppOptions["bundlerConfig"] = {
   viteOptions: {
@@ -16,7 +17,5 @@ export const bundlerConfig: AppOptions["bundlerConfig"] = {
         ]
       }
     },
-  },
-  chainWebpack: (config: WebpackChainConfig, isServer: boolean, isBuild: boolean) => {
   }
 }
