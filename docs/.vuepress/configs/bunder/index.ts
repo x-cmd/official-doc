12e524e7d7368caa203c2a7ccec1748de0156770
Zhengqbbb <1074059947@qq.com>
2021-12-27 22:29:22 +0800

    build(webpack): add webpack build

--- docs/.vuepress/configs/bunder/index.ts
+++ docs/.vuepress/configs/bunder/index.ts
@@ -1,13 +1,19 @@
 import type { AppOptions } from 'vuepress'
 const isProd = process.env.NODE_ENV === 'production'
 
-// export const bundler: AppOptions["bundler"] = // specify bundler via environment variable
-// process.env.DOCS_BUNDLER ??
-// // use vite in dev, use webpack in prod
-// (isProd ? '@vuepress/webpack' : '@vuepress/vite');
-export const bundler: AppOptions["bundler"] = '@vuepress/vite'
+export const bundler: AppOptions["bundler"] = process.env.DOCS_BUNDLER ??
+// use vite in dev, use webpack in prod
+(isProd ? "@vuepress/webpack" : "@vuepress/vite")
 
 export const bundlerConfig: AppOptions["bundlerConfig"] = {
+  postcss: {
+    postcssOptions:{
+      plugins: [
+        require('tailwindcss'),
+          require('autoprefixer')
+      ]
+    }
+  },
   viteOptions: {
     css: {
       postcss: {
